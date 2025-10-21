7c1d6af15e6f70c35db65c1b7cee86f1e327925a
volovales <124051396@upq.edu.mx>
2025-10-21 17:13:29 -0600

    PRACTICA10
    EN ESTA PRACTICA SE TRABAJO CON LAS IMAGENES DE FONDE O LAS COSAS DE FONDO

--- Intro/screens/ImageScreen.js
+++ Intro/screens/ImageScreen.js
@@ -1,11 +1,73 @@
-import { Text, View } from 'react-native'
+import { Text, StyleSheet, View,ImageBackground, Image, Dimensions } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
-export default function ImageScreen (){
+const backgroundImage = require("../assets/fondo.jpg");
 
-    return (
-      <View>
-        <Text>Proximamente.........</Text>
+export default function ImageScreen({navigation}){
+  const [showSplash, setShowSplash] = useState (true);
+  useEffect(() => {
+    const timer = setTimeout (()=>{
+      setShowSplash(false);
+    }, 3000);
+    return ()=> clearTimeout(timer);
+  }, []);
+  if (showSplash){
+    return(
+      <ImageBackground
+      source={backgroundImage}
+      style={styles.background}
+      resizeMode="cover"
+      >
+      <View style={styles.overlay}>
+        <Text style={styles.title}>Bienvenido</Text>
+                <Text style={styles.title}>Bienvenido</Text>
+                <Text style={styles.title}>Cargado....</Text>
       </View>
-    )
-  
+      </ImageBackground>
+    );
+  }
+
+  return(
+    <ImageBackground
+      source={backgroundImage}
+      style={styles.background}
+      resizeMode="cover"
+      >
+
+    <View style={styles.mainScreen}>
+      <Text style={styles.mainText}>Bienvenido </Text>
+    </View>
+    </ImageBackground>
+  );
 }
+  const {width,height} = Dimensions.get('window');
+  const styles = StyleSheet.create({
+    background:{
+      width: width,
+      height: height,
+    },
+    overlay:{
+      flex:1,
+//      backgroundColor: 'black',
+      justifyContent: 'center',
+      alignItems:'center',
+      padding:20,
+    },
+    title:{
+    color: '#fff',
+    fontSize:32,
+    fontWeight:'bold',
+    marginBlock:10,
+    textAlign:'center',
+    },
+    mainScreen:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+     //backgroundColor: '#f2f2f2',
+    },
+    mainText:{
+      fontSize:24,
+      color:'#333',
+    },
+  })
